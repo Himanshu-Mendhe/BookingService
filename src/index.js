981f30ec220328421350c9f81a8a2426df2e0753
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-18 16:14:03 +0530

    donen basic setup with dbsync, routes, env port

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/seerver-config');
 const app = express();
 
+const {PORT} = require('./config/seerver-config');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 
 const setupAndStartServer= () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    app.use('/api', apiRoutes);
 
     app.listen(PORT,() => {
         console.log(`server started on port no. --- ${PORT}---`);
-    })
+
+        if (process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    });
 } 
 
 setupAndStartServer();
