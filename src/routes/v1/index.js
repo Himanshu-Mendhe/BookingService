3914babed190e4731fefec6ff9d7c83d43056e90
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-23 16:18:53 +0530

    minor changes for api gateway setup testing

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post('/bookings', bookingController.create);
 router.post('/publish', bookingController.sendMessageToQueue);
 
-router.get('/routes', async(req,res) => {
+router.get('/info', async(req,res) => {
     res.status(200).json ({
         message:'inside routes v1 index to verify morgan gateway service'
     })
